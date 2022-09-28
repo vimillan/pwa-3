be4276d56b21a5bf5cf3dac909a08ca20e00b283
vimillan <20193tn066@utez.edu.mx>
2022-09-28 09:07:45 -0500

    verificacion de cahe

--- js/app.js
+++ js/app.js
@@ -2,3 +2,7 @@ if (navigator.serviceWorker) {
     console.log("El navegador soporta el Service Worker");
     navigator.serviceWorker.register("/pwa-3/sw.js");
 }
+
+if (window.caches) {
+    console.log("El navegador soporta cachés");
+}
