2500e856c013be53288a91670221297de4ffd77a
vimillan <20193tn066@utez.edu.mx>
2022-09-28 09:19:43 -0500

    cambio en addAll

--- sw.js
+++ sw.js
@@ -6,16 +6,16 @@ self.addEventListener("install", (event) => {
   console.log(INIT_MSG, "install");
   const promiseCache = caches.open('cache-v1.1').then((cache) => {
     return cache.addAll([
-      '/',
-      '/index.html',
-      '/pwa-3/images/lapras.png',
-      '/css/style.css',
+      './',
+      './index.html',
+      './pwa-3/images/lapras.png',
+      './css/style.css',
       'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
       'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
-      '/pages/Dividir.html',
-      '/pages/Sumar.html',
-      '/pages/Restar.html',
-      '/pages/Multiplicar.html',
+      './pages/Dividir.html',
+      './pages/Sumar.html',
+      './pages/Restar.html',
+      './pages/Multiplicar.html'
     ])
     /*return cache.addAll([
       '/',
