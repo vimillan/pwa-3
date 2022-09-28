87dbdb71a3ba5cd7f71c882019614c3291e21fed
vimillan <20193tn066@utez.edu.mx>
2022-09-28 09:27:36 -0500

    cambio en addAll para pages

--- sw.js
+++ sw.js
@@ -5,18 +5,6 @@ console.log("SW: Hola Mundo Violeta!");
 self.addEventListener("install", (event) => {
   console.log(INIT_MSG, "install");
   const promiseCache = caches.open('cache-v1.1').then((cache) => {
-    /*return cache.addAll([
-      './',
-      './index.html',
-      './pwa-3/images/lapras.png',
-      './css/style.css',
-      'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
-      'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
-      './pages/Dividir.html',
-      './pages/Sumar.html',
-      './pages/Restar.html',
-      './pages/Multiplicar.html'
-    ])*/
     return cache.addAll([
       '/pwa-3/',
       '/pwa-3/index.html',
