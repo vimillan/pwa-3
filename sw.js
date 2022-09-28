9fa3f88e804315a4651cac2b661570800f5567f5
vimillan <20193tn066@utez.edu.mx>
2022-09-28 09:13:59 -0500

    rutas alternas

--- sw.js
+++ sw.js
@@ -7,6 +7,18 @@ self.addEventListener("install", (event) => {
   const promiseCache = caches.open('cache-v1.1').then((cache) => {
     return cache.addAll([
       '/',
+      '/index.html',
+      '/pwa-3/images/lapras.png',
+      '/css/style.css',
+      'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
+      'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
+      '/pages/Dividir.html',
+      '/pages/Sumar.html',
+      '/pages/Restar.html',
+      '/pages/Multiplicar.html',
+    ])
+    /*return cache.addAll([
+      '/',
       '/pwa-3',
       '/pwa-3/index.html',
       '/pwa-3/images/lapras.png',
@@ -17,7 +29,7 @@ self.addEventListener("install", (event) => {
       '/pwa-3/pages/Sumar.html',
       '/pwa-3/pages/Restar.html',
       '/pwa-3/pages/Multiplicar.html',
-    ])
+    ])*/
   })
 
   event.waitUntil(promiseCache)
